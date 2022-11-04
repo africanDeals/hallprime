49de29119ea023e62277864baae94daa420d815b
Alain Herve <79479952+alainQtec@users.noreply.github.com>
2022-11-04 09:09:31 +0000

    Update packahes

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.remind
+  pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.replitPackages.jest
