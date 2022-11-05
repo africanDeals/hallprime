4e990e581895cf13d50d47ed9ee24c485fb2901e
Alain Herve <alain.1337dev@outlook.com>
2022-11-05 19:01:50 +0200

    back to cjs m

--- frontend/config/webpack/persistentCache/createEnvironmentHash.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-import { createHash } from "crypto";
-
-export default (env) => {
-  const hash = createHash("md5");
-  hash.update(JSON.stringify(env));
-
-  return hash.digest("hex");
-};
